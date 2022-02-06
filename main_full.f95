209843363dbb0b8f06fac4be78b51193a52845a7
joaaaaaaaaao <joao.lizarraga@gmail.com>
2022-02-05 19:30:04 -0500

    Changed overwriting variables

--- main_full.f95
+++ main_full.f95
@@ -6,86 +6,97 @@ use utils
 	! ****************************************
 	! 				Parameters
 	! ****************************************	
-	integer:: ix_al, ix_bt, k, l, q, r, t, j = 0, G = 50, tf = 500, B = 1000
-	integer, parameter:: M = 100, N = 1000		! Population matrix size
-	integer, dimension(M, N):: P, Pn, Pb	! Allocation(?)
+	integer:: ix_al, ix_bt, k, l, q, r, t, j = 1, G = 50, tf = 500 !, B = 1000
+	integer, parameter:: M = 100, N = 1000, B = 1000		! Population matrix size
+	integer, dimension(M, N):: P, Pn!, Pb	! Allocation(?)
 	integer, dimension(N):: al, bt, tmp, off	! Individuals
-	integer, dimension(M, M):: H 				! Hamming distances matrix
+	integer, dimension(M, M):: H, Hb 			! Hamming distances matrix
+	integer, dimension(M, B):: Pb
 	integer, allocatable:: C(:)					! Index of compatible individuals
-	allocate(C(0))
 
-	real:: r_n, mu = 0.1 						! Random number, mutation rate
+	real:: r_n, mu = 0.001 						! Random number, mutation rate
 	real, dimension(N):: r_v					! Random vector
 	real, dimension(M, N):: test_val			! Random matrix
 
+	!	allocate(C(0))
+
+
 	! Creates Population matrix for testing	
-	call random_number(test_val)
-	P = nint(test_val)
+	! call random_number(test_val)
+	! P = nint(test_val)
 	P = 0
 
 	! ****************************************
 	!              Main Loop
 	! ****************************************
-	open(1, file = "pop.dat") ! Opens .dat file 
-
-	do t = 0, tf
-
+	open(11, file = "pop.dat") ! Opens .dat file 
+	!open(12, file = "hamm.dat")	! TEST FILE
+	do t = 1, tf
 		Pb = P(:, 1:B)				! Matrix of mating segments
 		Hb = hamming_tr(Pb, M, B)   ! H. distances between mating segments
 
+		j = 1
 		do while (j <= M)
-
 			! Random index for 1st individual
 			call random_number(r_n)
-			ix_al = 1+ floor((M+ 1)*r_n)
+			ix_al = 1+ floor(M*r_n)
 
 			! 1st individual
 			al = P(ix_al, :)
 			
 			! Creates list of compatible individuals ----------
+			allocate(C(0))
 			do q = 1, M
 				if(Hb(ix_al, q) <= G)	C = [C, q]
 			end do 
+			
 			! -------------------------------------------------
 			
-			if (size(A) /= 0) then
+			if (size(C) /= 0) then
+				
 				call random_number(r_n)
-				ix_bt = C(1+ floor(r_n*size(C))) ! Random index for beta from the compatible individuals list
+				ix_bt = C(1 + floor(r_n*size(C))) ! Random index for beta from the compatible individuals list
+				
 
 				! 2nd individual
 				bt = P(ix_bt, :)
 
-				! Offspring + mutation
+				! Recombination + mutation
 				call random_number(r_v)
 				tmp = floor(r_v*2)
 
-				call random_number(r_n)
-				off(l) = (tmp(l)*al(l)+ (1 - tmp(l))*bt(l) - merge(1, 0, r_n < mu))**2 
 				! do l = 1, N
 				! 	call random_number(r_n)
+				!	off(l) = (tmp(l)*al(l)+ (1 - tmp(l))*bt(l) - merge(1, 0, r_n < mu))**2 
+				!	! if (merge(1, 0, r_n < mu) == 1) print*, "j: ", j, "l: ", l, "Mutó"
+				! end do
+				 do l = 1, N
+				 	call random_number(r_n)
 					
-				! 	! off(l) = (tmp(l)*al(l)+ (1 - tmp(l))*bt(l) - merge(1, 0, r_n < mu))**2 
-				! 	off(l) = tmp(l)*al(l)+ (1 - tmp(l))*bt(l)
-				! 	if (r_n < mu)	off(l) = 1 - off(l)
+				 	! off(l) = (tmp(l)*al(l)+ (1 - tmp(l))*bt(l) - merge(1, 0, r_n < mu))**2 
+				 	off(l) = tmp(l)*al(l)+ (1 - tmp(l))*bt(l)
+				 	if (r_n < mu)	off(l) = 1 - off(l)
+				 	
+				 end do 
+				
+				! print*, t, size(off)
+				! Fills new population
+				Pn(j, :) = off
+				j = j + 1
+			end if
 
-				! end do 
-
-			end if 
-			! Fills new population
-			Pn(j, :) = off
-			j = j + 1
+			deallocate(C)
 			
-			! print*, j 
-
 		end do ! End while
 		P = Pn
 	
-	print*, t, "Out of ", tf
-	end do
+	! print*, t, "Out of ", tf
+	end do ! End generations loop
 
-	! H = hamming_tr(P, M, N)
+	!H = hamming_tr(P, M, N)
 	do r = 1, M
-		write(1, *) P(r, :)
+		write(11, *) P(r, :)
+	!	write(12, *) H(r, :)
 	end do
 
 end program main_full
